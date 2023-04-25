9f14edbd142ceaf358b5231970af0fa3cd2f8e08
Jaralagus <126630349+Jaralagus@users.noreply.github.com>
2023-04-25 13:05:21 -0300

    Add files via upload

---
+++ Curso Preparatorio Henry/clases.js
@@ -0,0 +1,30 @@
+//function Auto (puertas, color, marca, año, ruedas) {
+    //this.puertas= puertas;
+    //this.color=color;
+    //this.marca=marca;
+    //this.año=año;
+    //this.ruedas=ruedas;
+    //this.informacion = function () {
+        //console.log ('Este es un ' + this.marca + ' de color ' + this.color);
+   // };
+//}
+
+//var miPrimerAuto = new Auto (2, 'Rojo', 'Ferrari', 1990, 4);
+//miPrimerAuto.informacion ();
+
+//console.log (miPrimerAuto);
+//console.log (miPrimerAuto.marca);
+
+//class Auto {
+    //constructor (puertas, color, marca, año, ruedas) {
+    //this.puertas= puertas;
+    //this.color=color;
+    //this.marca=marca;
+   // this.año=año;
+    //this.ruedas=ruedas;
+   // }
+//}
+
+   // var miSegundoAuto = new Auto (4, 'Blanco', 'Fiat', 2015, 4);
+    //console.log (miSegundoAuto);
+    //console.log (miSegundoAuto.marca);
